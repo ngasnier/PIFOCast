a50b8ec54eb4129c3e3127471e00cc4eb35020e8
Nicolas Gasnier <ngasnier@orange.fr>
2025-06-09 11:33:16 +0200

    dataset download, creation and train with validation dataset

--- era5.py
+++ era5.py
@@ -1,16 +1,14 @@
 import cdsapi
 
 # https://cds.climate.copernicus.eu/datasets/reanalysis-era5-pressure-levels?tab=overview
-
-
-#Hersbach, H., Bell, B., Berrisford, P., Biavati, G., Horányi, A., Muñoz Sabater, J., Nicolas, J., Peubey, C., Radu, R., Rozum, I., Schepers, D., Simmons, A., Soci, C., Dee, D., Thépaut, J-N. (2023): ERA5 hourly data on pressure levels from 1940 to present. Copernicus Climate Change Service (C3S) Climate Data Store (CDS), DOI: 10.24381/cds.bd0915c6 (Accessed on DD-MMM-YYYY)
+# Hersbach, H., Bell, B., Berrisford, P., Biavati, G., Horányi, A., Muñoz Sabater, J., Nicolas, J., Peubey, C., Radu, R., Rozum, I., Schepers, D., Simmons, A., Soci, C., Dee, D., Thépaut, J-N. (2023): ERA5 hourly data on pressure levels from 1940 to present. Copernicus Climate Change Service (C3S) Climate Data Store (CDS), DOI: 10.24381/cds.bd0915c6 (Accessed on DD-MMM-YYYY)
 
 # $HOME/.cdsapirc :
 # url: https://cds.climate.copernicus.eu/api
 # key: e57c3cd8-3c0a-4471-9460-b503ea778230
 
 #dataset = "reanalysis-era5-single-levels"
-dataset = "reanalysis-era5-pressure-levels"
+era5dataset = "reanalysis-era5-pressure-levels"
 # request = {
 #     "product_type": ["reanalysis"],
 #     "variable": ["geopotential"],
@@ -25,44 +23,53 @@ dataset = "reanalysis-era5-pressure-levels"
 #     "data_format": "grib",
 #     "download_format": "unarchived"
 # }
-request = {
-    "product_type": ["reanalysis"],
-    "variable": [
-        "geopotential",
-        "u_component_of_wind",
-        "v_component_of_wind"
-    ],
-    "year": ["2025"],
-    "month": ["04"],
-    "day": [
-        "01", "02", "03",
+
+years = ["2024"]
+monthes = ["01", "02", "03",
         "04", "05", "06",
         "07", "08", "09",
-        "10", "11", "12",
-        "13", "14", "15",
-        "16", "17", "18",
-        "19", "20", "21",
-        "22", "23", "24",
-        "25", "26", "27",
-        "28", "29", "30"
-    ],
-    "time": [
-        "00:00", "06:00", "12:00",
-        "18:00"
-    ],
-    "pressure_level": ["500"],
-    "data_format": "grib",
-    "download_format": "unarchived"
-}
+        "10", "11", "12"]
+days = [
+    "01", "02", "03",
+    "04", "05", "06",
+    "07", "08", "09",
+    "10", "11", "12",
+    "13", "14", "15",
+    "16", "17", "18",
+    "19", "20", "21",
+    "22", "23", "24",
+    "25", "26", "27",
+    "28", "29", "30",
+    "31"
+]
+times = [
+    "00:00", "06:00",
+    #"12:00", "18:00"
+]
 
-target = 'dataset/202504.grib'
+for year in years:
+    for month in monthes:
+        request = {
+            "product_type": ["reanalysis"],
+            "variable": [
+                "geopotential",
+                "u_component_of_wind",
+                "v_component_of_wind"
+            ],
+            "year": [year],
+            "month": [month],                
+            "day": days,
+            "time": times,
+            "pressure_level": ["500"],
+            "data_format": "grib",
+            "download_format": "unarchived"
+        }
 
-client = cdsapi.Client()
-client.retrieve(dataset, request, target)
+        target = 'dataset/'+year+month+'.grib'
+
+        client = cdsapi.Client()
+        client.retrieve(dataset, request, target)
 
 
-
-
-
-client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+# client = cdsapi.Client()
+# client.retrieve(dataset, request).download()
--- pifocast/data.py
+++ pifocast/data.py
@@ -23,6 +23,7 @@ def normalizeField(field):
 def pifoGridGenerator(grib_file: str, 
                       grid:LatLonGrid,
                       stride:int = 4,
+                      step:int = 1,
                       **task_kwargs):
     """
     Generator of graph and labels for training.
@@ -30,6 +31,7 @@ def pifoGridGenerator(grib_file: str,
     grib_file : a ERA5 grib file path, with Z, U and V fields at 500hPa.
     grid : a LatLonGrid with corresponding shape.
     stride : default 4. To reduce resolution of the grib data.
+    step : default 1. Defines how to advance in grib file to generate examples.
     Yields :
     GraphTensor of pifo.pbtxt schema, with corresponding normalized features on nodes and edges
     Tensor concatenating Z, U, V normalized features of prediction, according to the LatLonGrid parameters.
@@ -38,18 +40,12 @@ def pifoGridGenerator(grib_file: str,
     ds = xr.load_dataset(grib_file, engine="cfgrib", decode_timedelta=False)
     # Sélection d'une grille réduite avec un sous-échantillonnage (1 point sur 4)
     ds_reduced = ds.isel(latitude=slice(0, None, stride), longitude=slice(0, None, stride))
-    
+
     G_NLat = ds_reduced.latitude.shape[0]
     G_NLon = ds_reduced.longitude.shape[0]
     assert G_NLat==grid.NLat and G_NLon==grid.NLon, "grid definition and grib file mismatch."
 
     NExamples = ds_reduced.z.shape[0]
-    # Nfeatures = 3
-    # NEdgeFeatures = 3
-  
-    # cos_lat, _, _ = normalizeField(tf.math.sin(grid.lat2d*math.pi/180))
-    # cos_lon, _, _ = normalizeField(tf.math.cos(grid.lon2d*math.pi/180))
-    # sin_lon, _, _ = normalizeField(tf.math.sin(grid.lon2d*math.pi/180))
 
     # Create train data
     Z = tf.reshape(tf.convert_to_tensor(ds_reduced.z[0:NExamples]), [NExamples, grid.NNodes])
@@ -61,19 +57,8 @@ def pifoGridGenerator(grib_file: str,
     v = tf.reshape(tf.convert_to_tensor(ds_reduced.v[0:NExamples]), [NExamples, grid.NNodes])
     v, _, _ = normalizeField(v)
 
-    # edge_lengthes = tf.convert_to_tensor(grid.lengthes, dtype_hint="float")
-    # edge_lengthes, _, _ = normalizeField(edge_lengthes)
-
-    # x, _, _ = normalizeField(tf.convert_to_tensor(grid.coords_x, dtype_hint="float"))
-    # y, _, _ = normalizeField(tf.convert_to_tensor(grid.coords_y, dtype_hint="float"))
-    # edgeFeatures = tf.stack([
-    #         edge_lengthes, 
-    #         x,
-    #         y
-    #     ], axis=1)
-    
-
-    for i in range(m-1): # To allow for making Y
+    for i in range(0, m-1, step): # To allow for making Y
+        print("    grib index ", i)
         features = tf.stack([Z[i], u[i], v[i]], axis=1)  # , cos_lat, cos_lon, sin_lon
         predictions = tf.stack([Z[i+1], u[i+1], v[i+1]], axis=1)        
         yield getGraphForFeatures(features, grid), predictions  #  buildGridGNN(features, edgeFeatures, grid)
@@ -85,56 +70,6 @@ def getGraphExample(grid:LatLonGrid, file="dataset/202504.grib"):
     """
     generator = pifoGridGenerator(file, grid)
     return next(generator)
-    #     # Load data (8 z500 ggrib_file...)
-    # ds = xr.load_dataset("dataset/202504.grib", engine="cfgrib", decode_timedelta=False)
-    # #ds_reduced = ds
-    # # Sélection d'une grille réduite avec un sous-échantillonnage (1 point sur 4)
-    # ds_reduced = ds.isel(latitude=slice(0, None, 4), longitude=slice(0, None, 4))
-
-    # G_NLat = ds_reduced.latitude.shape[0]
-    # G_NLon = ds_reduced.longitude.shape[0]
-    # assert G_NLat==grid.NLat and G_NLon==grid.NLon, "grid definition and grib file mismatch."
-
-    # NEdgeFeatures = 3
-
-    # # Create train data
-    # NExamples = ds_reduced.z.shape[0]
-
-    # Z = tf.reshape(tf.convert_to_tensor(ds_reduced.z[0:NExamples-1]), [NExamples-1, grid.NNodes])
-    # Z, _ , _ = normalizeField(Z)
-
-    # u = tf.reshape(tf.convert_to_tensor(ds_reduced.u[0:NExamples]), [NExamples, grid.NNodes])
-    # u, _, _ = normalizeField(u)
-    # v = tf.reshape(tf.convert_to_tensor(ds_reduced.v[0:NExamples]), [NExamples, grid.NNodes])
-    # v, _, _ = normalizeField(v)
-
-    # cos_lat, _, _ = normalizeField(tf.math.sin(grid.lat2d*math.pi/180))
-    # cos_lon, _, _ = normalizeField(tf.math.cos(grid.lon2d*math.pi/180))
-    # sin_lon, _, _ = normalizeField(tf.math.sin(grid.lon2d*math.pi/180))
-
-    
-    # features = tf.stack([Z[0], u[0], v[0], cos_lat, cos_lon, sin_lon], axis=1)
-
-    # #Nfeatures = 3
-    # # edgeFeatures = tf.zeros([grid.NEdges, NEdgeFeatures], dtype=tf.float32)
-    # edge_lengthes = tf.convert_to_tensor(grid.lengthes, dtype_hint="float")
-    # edge_lengthes, _, _ =  normalizeField(edge_lengthes)
-    # x, _, _ = normalizeField(tf.convert_to_tensor(grid.coords_x, dtype_hint="float"))
-    # y, _, _ = normalizeField(tf.convert_to_tensor(grid.coords_y, dtype_hint="float"))
-    
-    # edgeFeatures = tf.stack([
-    #         edge_lengthes, 
-    #         x,
-    #         y
-    #     ], axis=1)
-
-    # # for i in range(10):
-    # #     ds_reduced.z[i].plot()
-    # #     plt.savefig("X_"+str(i)+".png", dpi=150, bbox_inches="tight")  # Ajuste la qualité et l'encadrement
-    # #     plt.close()  
-    # # ds_reduced.z[1].plot()
-    
-    # return buildGridGNN(features, edgeFeatures, grid), tf.stack([Z[0], u[0], v[0]])
 
 
 # Quick & dirty way of chaining predictions with ...
